1eb9b84997ebdf334a17720c4496db7cc39f5253
Arceus-sj <71997730+Arceus-sj@users.noreply.github.com>
2021-08-01 20:33:03 +0530

    CodeChef Beginner Practise Problem
    link: https://www.codechef.com/problems/FLOW001

---
+++ CodeChef_Submission/addTwoNum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        ll int a, b;
+        cin >> a >> b;
+
+        ll int sum = (a + b);
+
+        cout << sum << endl;
+    }
+    return 0;
+}
