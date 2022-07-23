5eada3df1a837d52bdfc339dc8a7cca178c18d78
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-07-23 23:49:34 +0530

    Long Challenge II | Problem Code: INSURANCE

---
+++ CodeChef_Submission/insurance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int x, y;
+        cin >> x >> y;
+        
+        if(y <= x) {
+            cout << y << "\n";
+        }
+        else {
+            cout << x << "\n";
+        }
+    }
+    return 0;
+}
