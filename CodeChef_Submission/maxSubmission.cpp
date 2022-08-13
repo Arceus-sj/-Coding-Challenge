74690b4ff7fb674bfed820efdc3a21fdfd394964
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-08-13 23:45:50 +0530

    August Long 2022 | Problem Code: MAXIMUMSUBS

---
+++ CodeChef_Submission/maxSubmission.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	
+	while(t--) {
+	    int x;
+	    cin >> x;
+	    
+	    cout << x*2 << "\n";
+	}
+	return 0;
+}
