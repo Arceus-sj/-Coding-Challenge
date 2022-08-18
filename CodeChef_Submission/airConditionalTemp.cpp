dc81c95d83b5df4808a991770635fd136cba3360
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-08-18 09:44:53 +0530

    starter 52 | Problem Code: ACTEMP

---
+++ CodeChef_Submission/airConditionalTemp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--) {
+	    int a, b, c;
+	    cin >> a >> b >> c;
+	    
+	    if(a <= b && c <= b) {
+	        cout << "Yes" << "\n";
+	    }
+	    else {
+	        cout << "No" << "\n";
+	    }
+	    
+	    
+	}
+	return 0;
+}
