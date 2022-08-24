640c579e8bbbcc540908310cee118909aa1c7cea
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-08-24 23:47:00 +0530

    Starter 53 | Problem Code: AVGPROBLEM

---
+++ CodeChef_Submission/avgproblem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t; cin >> t;
+	while(t--) {
+		double a, b, c;
+		cin >> a >> b >> c;
+
+		if((a+b)/2 > c)
+			cout << "YES" << "\n";
+		else
+			cout << "NO" << "\n";
+	}
+	return 0;
+}
