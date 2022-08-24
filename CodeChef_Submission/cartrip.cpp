438fea94d9fd7e326016c77f84fb9a3d6be6a935
Shantanu Jana <71997730+Arceus-sj@users.noreply.github.com>
2022-08-24 23:46:05 +0530

    Starter 53 | Problem Code: CARTRIP

---
+++ CodeChef_Submission/cartrip.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin >> t;
+
+	while(t--) {
+		int x; cin >> x;
+
+		if(x < 300) {
+			cout << 3000 << "\n";
+		}
+		else {
+			cout << x*10 << "\n";
+		}
+	}
+
+
+	return 0;
+}
